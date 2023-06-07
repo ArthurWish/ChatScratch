cec7533223a3e0cd1d8caff3955142cf320fede4
YunNong <1223174891@qq.com>
2023-06-07 11:01:43 +0800

    code lint

--- tools.py
+++ tools.py
@@ -9,6 +9,8 @@ from chat import create_chat_completion
 import openai
 from base64 import b64decode
 from PIL import Image
+import os
+import hashlib
 
 PROMPT = """
 {"prompt":"点击角色，使角色变色 ->","completion":" \"when this sprite clicked\",\"change [color] effect by [25]\"\n"}
@@ -73,6 +75,8 @@ PROMPT = """
 {"prompt":"点击绿旗，让角色到舞台顶端 ->","completion":" “when green flag clicked\",“go to [random position]\",\"set y to [180]\"\n"}
 {"prompt":"点击绿旗，累计分数 ->","completion":" “when green flag clicked\",“set [Score] to [0]\",“forever”,\"if touching [Bowl] ? then\",\"play sound [pop] until done\",\"change [Score] by [1]\",\"go to [random position]\",\"set y to [180]\"\n"}
 {"prompt":"点击绿旗，设置奖励分数 ->","completion":" “when green flag clicked\",“set [Score] to [0]\",“forever”,\"if touching [Bowl] ? then\",\"play sound [pop] until done\",\"change [Score] by [1]\",\"go to [random position]\",\"set y to [180]\"\n"}"""
+
+MODEL = "gpt-3.5-turbo"
 
 
 def extract_keywords(s) -> List:
@@ -141,6 +145,33 @@ def question_and_relpy(question):
     # messages.append({"role": "assistant", "content": agent_reply})
     return agent_reply
 
+def generate_draw(drawing_type, drawing_content, save_path):
+    temp_memory = []
+    if drawing_type == "role":
+        temp_memory.append({
+            "role":
+            "user",
+            "content":
+            f"""你是人工智能程序的提示生成器。你的工作是提供简短的一句话描述。这里有一个关于动画风格角色的描述<{drawing_content}>。使用类似英文生成提示："Anime-style human with a transparent background, exploring a magical world with a butterfly companion. Line art, anime, colored, child style."。翻译为英语。
+        """
+        })
+    elif drawing_type == "background":
+        temp_memory.append({
+            "role":
+            "user",
+            "content":
+            f"""你是人工智能程序的提示生成器。你的工作是提供简短的一句话描述。这里有一个关于动画风格背景的描述<{drawing_content}>，你需要根据<描述>决定：场景发生的地方，重点的景物。使用类似英文生成提示："Forest with running track at the end, featuring trees and a running track. Line art, anime, colored, child style."。翻译为英语。
+        """
+        })
+    else:
+        raise "Not valid drawing type"
+    # print(temp_memory)
+    agent_reply = create_chat_completion(model=MODEL,
+                                         messages=temp_memory,
+                                         temperature=0)
+    print("agent: ", agent_reply)
+    image_data = generate_draw_with_dalle(agent_reply, save_path)
+    return image_data
 
 def generate_draw_with_dalle(prompt, save_path):
     # image_data_list = []
@@ -273,6 +304,179 @@ def split_to_parts(reply: str):
     # # print(result)
     # return result
 
+def toSVG(infile):
+    image = Image.open(infile).convert('RGBA')
+    data = image.load()
+    width, height = image.size
+    svg_str = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n'
+    svg_str += '<svg id="svg2" xmlns="http://www.w3.org/2000/svg" version="1.1" \
+                width="%(x)i" height="%(y)i" viewBox="0 0 %(x)i %(y)i">\n' % \
+              {'x': width, 'y': height}
+    for y in range(height):
+        for x in range(width):
+            rgba = data[x, y]
+            rgb = '#%02x%02x%02x' % rgba[:3]
+            if rgba[3] > 0:
+                svg_str += '<rect width="1" height="1" x="%i" y="%i" fill="%s" \
+                    fill-opacity="%.2f" />\n' % (x, y, rgb, rgba[3]/255.0)
+    svg_str += '</svg>\n'
+    return svg_str
+
+# 计算字符串的MD5哈希值的函数
+def get_str_md5(input_str):
+    md5_obj = hashlib.md5()
+    md5_obj.update(input_str.encode())
+    return md5_obj.hexdigest()
+
+
+def generate_js():
+    backdrop_files = os.listdir('static/scene')
+    costume_files = os.listdir('static/role')
+   
+    with open('C:/Users/11488/Desktop/react_try/scratch-gui/src/lib/default-project/index.js', 'w') as f:
+        f.write('import projectData from \'./project-data\';\n')
+        for i, file in enumerate(backdrop_files):
+            f.write('import backdrop' + str(i + 1) + ' from \'!raw-loader!./' + file + '\';\n')
+        for i, file in enumerate(costume_files):
+            f.write('import costume' + str(i + 1) + ' from \'!raw-loader!./' + file + '\';\n')
+
+        f.write('\nconst defaultProject = translator => {\n')
+        f.write('let _TextEncoder;\n')
+        f.write('if (typeof TextEncoder === \'undefined\') {\n')
+        f.write('    _TextEncoder = require(\'text-encoding\').TextEncoder;\n')
+        f.write('} else {\n')
+        f.write('    _TextEncoder = TextEncoder;\n')
+        f.write('}\n')
+        f.write('const encoder = new _TextEncoder();\n\n')
+        f.write('const projectJson = projectData(translator);\n\n')
+        f.write('    return [{\n')
+        f.write('        id: 0,\n')
+        f.write('        assetType: \'Project\',\n')
+        f.write('        dataFormat: \'JSON\',\n')
+        f.write('        data: JSON.stringify(projectJson)\n')
+        f.write('    },\n')
+
+        for i, file in enumerate(backdrop_files):
+            f.write('    {\n')
+            f.write('        id: \'' + file.split('.')[0] + '\',\n')
+            f.write('        assetType: \'ImageVector\',\n')
+            f.write('        dataFormat: \'SVG\',\n')
+            f.write('        data: encoder.encode(backdrop' + str(i + 1) + ')\n')
+            f.write('    },\n')
+        
+        for i, file in enumerate(costume_files):
+            f.write('    {\n')
+            f.write('        id: \'' + file.split('.')[0] + '\',\n')
+            f.write('        assetType: \'ImageVector\',\n')
+            f.write('        dataFormat: \'SVG\',\n')
+            f.write('        data: encoder.encode(costume' + str(i + 1) + ')\n')
+            f.write('    },\n')
+
+        f.write('];\n};\n\nexport default defaultProject;\n')
+        
+        
+def generate_js_project():
+    backdrop_files = os.listdir('static/scene')
+    costume_files = os.listdir('static/role')
+
+    with open('C:/Users/11488/Desktop/react_try/scratch-gui/src/lib/default-project/project-data.js', 'w') as f:
+        f.write('import {defineMessages} from \'react-intl\';\n')
+        f.write('import sharedMessages from \'../shared-messages\';\n\n')
+        f.write('let messages = defineMessages({\n')
+        f.write('    meow: {\n')
+        f.write('        defaultMessage: \'Meow\',\n')
+        f.write('        description: \'Name for the meow sound\',\n')
+        f.write('        id: \'gui.defaultProject.meow\'\n')
+        f.write('    },\n')
+        f.write('    variable: {\n')
+        f.write('        defaultMessage: \'my variable\',\n')
+        f.write('        description: \'Name for the default variable\',\n')
+        f.write('        id: \'gui.defaultProject.variable\'\n')
+        f.write('    }\n')
+        f.write('});\n\n')
+        f.write('messages = {...messages, ...sharedMessages};\n\n')
+        f.write('const defaultTranslator = msgObj => msgObj.defaultMessage;\n\n')
+        
+        f.write('/**\n')
+        f.write(' * Generates the project data.\n')
+        f.write(' * @param {function} translateFunction - A function to translate the text in the project.\n')
+        f.write(' * @return {object} The project data with multiple targets each with its own properties.\n')
+        f.write(' */\n')
+        
+        f.write('const projectData = translateFunction => {\n')
+        f.write('    const translator = translateFunction || defaultTranslator;\n')
+        f.write('    return ({\n')
+        f.write('        targets: [\n')
+
+        # for i, file in enumerate(backdrop_files):
+        f.write('            {\n')
+        f.write('                isStage: true,\n')
+        f.write('                name: \'Stage\',\n')
+        f.write('                variables: {\n')
+        f.write('                    \'`jEk@4|i[#Fk?(8x)AV.-my variable\': [\n')
+        f.write('                        translator(messages.variable),\n')
+        f.write('                        0\n')
+        f.write('                    ]\n')
+        f.write('                },\n')
+        f.write('                lists: {},\n')
+        f.write('                broadcasts: {},\n')
+        f.write('                blocks: {},\n')
+        f.write('                currentCostume: 0,\n')
+        f.write('                costumes: [\n')
+        for i, file in enumerate(backdrop_files):
+            f.write('                    {\n')
+            f.write('                        assetId: \'' + file.split('.')[0] + '\',\n')
+            f.write('                        name: translator(messages.backdrop, {index: ' + str(i+1) + '}),\n')
+            f.write('                        md5ext: \'' + file + '\',\n')
+            f.write('                        dataFormat: \'svg\',\n')
+            f.write('                        rotationCenterX: 500,\n')
+            f.write('                        rotationCenterY: 460\n')
+            f.write('                    },\n')
+        f.write('                ],\n')
+        f.write('                sounds: [],\n')
+        f.write('                volume: 100\n')
+        f.write('            },\n')
+        for i, file in enumerate(costume_files):
+            f.write('            {\n')
+            f.write('                isStage: false,\n')
+            f.write('                name: translator(messages.sprite, {index: ' + str(i + 1) + '}),\n')
+            f.write('                variables: {},\n')
+            f.write('                lists: {},\n')
+            f.write('                broadcasts: {},\n')
+            f.write('                blocks: {},\n')
+            f.write('                currentCostume: 0,\n')
+            f.write('                costumes: [\n')
+        
+            f.write('                    {\n')
+            f.write('                        assetId: \'' + file.split('.')[0] + '\',\n')
+            f.write('                        name: translator(messages.costume, {index: ' + str(1) + '}),\n')
+            f.write('                        bitmapResolution: 1,\n')
+            f.write('                        md5ext: \'' + file + '\',\n')
+            f.write('                        dataFormat: \'svg\',\n')
+            f.write('                        rotationCenterX: 500,\n')
+            f.write('                        rotationCenterY: 460\n')
+            f.write('                    },\n')
+            f.write('                ],\n')
+            f.write('                sounds: [],\n')
+            f.write('                volume: 100,\n')
+            f.write('                visible: true,\n')
+            f.write('                x: 0,\n')
+            f.write('                y: 0,\n')
+            f.write('                size: 100,\n')
+            f.write('                direction: 90,\n')
+            f.write('                draggable: false,\n')
+            f.write('                rotationStyle: \'all around\'\n')
+            f.write('            },\n')
+
+        f.write('        ],\n')
+        f.write('        meta: {\n')
+        f.write('            semver: \'3.0.0\',\n')
+        f.write('            vm: \'0.1.0\',\n')
+        f.write('            agent: \'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36\'\n')
+        f.write('        }\n')
+        f.write('    });\n')
+        f.write('};\n\n')
+        f.write('export default projectData;\n')
 
 def test():
     string = '1. 小明：一个勇敢的男孩，喜欢探险和冒险。他在森林里迷路了，正在寻找回家的路。\n2. 小芳：一个聪明的女孩，喜欢读书和学习。她在图书馆里发现了一本神秘的书，决定破解其中的谜题。\n3. 小华：一个善良的男孩，喜欢帮助别人。他在街上看到一个老奶奶摔倒了，决定去帮助她。\n4. 小玲：一个有想象力的女孩，喜欢画画和创作。她在花园里发现了一只神奇的小鸟，决定和它成为朋友。'
